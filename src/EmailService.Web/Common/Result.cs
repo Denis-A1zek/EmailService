e22aa4c8692e59e791929da1560c24b55fd44d68
Denis-A1zek <denissigida.work@gmail.com>
2024-01-17 17:55:54 +0300

    result patter

---
+++ src/EmailService.Web/Common/Result.cs
@@ -0,0 +1,27 @@
+﻿namespace EmailService.Web.Common;
+
+public sealed record Result<T>
+{
+    private Result(T value)
+    {
+        IsSuccess = true;
+        Value = value;
+        Error = null;
+    }
+
+    private Result(string error)
+    {
+        IsSuccess = false;
+        Value = default(T);
+        Error = error;
+    }
+    public bool IsSuccess { get; }
+    public T Value { get; }
+    public string Error { get; }
+
+    public static Result<T> Ok(T value)
+        => new Result<T>(value);
+
+    public static Result<T> Fail(string error)
+        => new Result<T>(error);
+}
