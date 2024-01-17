e97b165d640f1e842c99fdd123895f94ec191a1c
Denis-A1zek <denissigida.work@gmail.com>
2024-01-17 13:47:38 +0300

    Options patter is implemented

---
+++ src/EmailService.Core/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmailService.Core;
+
+public static class Extensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IEmailSender, EmailSender>();
+
+        services.AddOptions<SmtpOptions>().BindConfiguration(nameof(SmtpOptions));
+
+        return services;
+    }
+}
