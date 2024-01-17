f068a96e522370dde5903fc4e24697b220cb9529
Denis-A1zek <denissigida.work@gmail.com>
2024-01-17 13:47:46 +0300

    web

---
+++ src/EmailService.Web/Controllers/MailsController.cs
@@ -0,0 +1,17 @@
+﻿using EmailService.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+
+namespace EmailService.Web.Controllers;
+
+[ApiController]
+[Route("api/mails")]
+public class MailsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<string>> Get()
+    {
+        
+        return Ok("dfs");
+    }
+}
